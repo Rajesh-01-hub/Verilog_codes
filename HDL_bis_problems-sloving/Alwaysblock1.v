328c33480a5370c8e828d4d2a53f3ef896feab5f
Avula_Rajesh <rajeshep01avula@gmail.com>
2025-07-27 20:53:13 +0530

    Create Alwaysblock1.v
    Since digital circuits are composed of logic gates connected with wires, any circuit can be expressed as some combination of modules and assign statements. However, sometimes this is not the most convenient way to describe the circuit. Procedures (of which always blocks are one example) provide an alternative syntax for describing circuits.
    
    For synthesizing hardware, two types of always blocks are relevant:
    
    Combinational: always @(*)
    Clocked: always @(posedge clk)
    Combinational always blocks are equivalent to assign statements, thus there is always a way to express a combinational circuit both ways. The choice between which to use is mainly an issue of which syntax is more convenient. The syntax for code inside a procedural block is different from code that is outside. Procedural blocks have a richer set of statements (e.g., if-then, case), cannot contain continuous assignments*, but also introduces many new non-intuitive ways of making errors.

---
+++ HDL_bis_problems-sloving/Alwaysblock1.v
@@ -0,0 +1,13 @@
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign = a & b; // combitional circuit continous assigment wire
+    always @(*) begin
+        out_alwaysblock = a & b; // reg value output use behavioural level
+    end
+// getting the same logic difference is here , logic write in inside the producre , logic write in output 
+endmodule
