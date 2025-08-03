bb6767827f48fd57a41a6f049ae8083055c1dca5
Avula_Rajesh <rajeshep01avula@gmail.com>
2025-08-03 11:24:12 +0530

    Create encoder_8X3.v
    If more than one input is active — it's invalid for basic encoders.
    simple/Basic Encoder---->Only one input active at a time; no handling of multiple inputs.
    
    Think of the encoder as a "number translator":
    
    Inputs are like buttons.
    
    Outputs are the binary code of whichever button is pressed.
    
    If you press only one button → fine.
    If you press multiple buttons at once, the encoder may confuse or misbehave unless it’s a priority encoder.

---
+++ Arthematic_logic_combinational_circuits/encoder_8X3.v
@@ -0,0 +1,9 @@
+// normal encoder only can accept 1 input at a time , it does not consider multiple bits at a time 
+// due to reason we used priproty encoder
+// Here , build 8X1 encoder .
+
+module encode_8X3(input [7:0] in, output [2:0]out);
+  assign out[0] = in[1] | in[3] | in[5] | in[7];
+  assign out[1] = in[2] | in[3] | in[6] | in[7];
+  assign out[2] = in[4] | in[5] | in[6] | in[7];
+endmodule
